4826410d0c8a95b8b230bac3133c16823351496e
Hansard-T <72332474+Hansard-T@users.noreply.github.com>
2020-10-31 17:29:01 +0800

    Update test10.c

--- test10.c
+++ test10.c
@@ -9,38 +9,43 @@ int main()
     double average1[5];
     double sum,sum1;
     
-    while(scanf("%d  %d",&n,&m)!=EOF ){
+    while(scanf("%d  %d",&n,&m)!=EOF )
+    {
         number=0;
 
     for(int a=0;a<n;a++)
         for(int b=0; b<m;b++)
             scanf("%lf",&v[a][b]);
 		
-    for(int a=0;a<n;+a++){
+    for(int a=0;a<n;+a++)
+    {
         sum=0;
         for(int b=0;b<m;b++)
             sum += v[a][b];
             average[a]=sum/m;
-}
-    for(int b=0; b<m;b++){
+    }
+    for(int b=0; b<m;b++)
+    {
         sum= 0;
         for(int a=0;a<n;a++)
             sum1 += v[a][b];
             average1[b] = sum1/n;
-}
-    for(int a=0;a<n;a++){
+    }
+    for(int a=0;a<n;a++)
+    {
         int i=1;
-            for(int b=0;b<m;b++){
+            for(int b=0;b<m;b++)
+	    {
                 if(v[a][b]<average1[b])
                     i=0;
-}
+            }
      if(i==1) 
          number++;
-}
+    }
 
-    printf( "%.2lf",average[0] );
+    printf("%.2lf",average[0]);
     for(int a=1;a<n;a++)
-        printf("%.2lf",average[a] );
+        printf("%.2lf",average[a]);
         putchar('\n');
 
     printf("%.2lf",average1[0] );
@@ -49,6 +54,6 @@ int main()
             putchar('\n');
 		
     printf("%d\n\n",number);
-}
+    }
     return 0;
 }
